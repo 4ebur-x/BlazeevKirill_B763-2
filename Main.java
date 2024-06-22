55a233fe902ddf22569782b6b0a58d000e1b850e
4ebur-x <kirillblazeev10@gmail.com>
2024-06-22 22:00:19 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,74 @@
+import java.security.PublicKey;
+import java.util.PrimitiveIterator;
+
+class Main{
+    public static void main(String[] args){
+
+        Car car = new Car();
+        car.checkMark();
+        car.refuel();
+        car.changeWheel();
+        car.drive();
+    }
+}
+
+
+
+
+class Engine{
+        private String engine;
+        public Engine(String engine ){
+            this.engine = engine;
+        }
+        public void checkEngine(){
+            System.out.println("Двигатель в машине - " + engine);
+        }
+
+}
+
+
+
+
+class Wheel{
+        private int wheel;
+        public Wheel(int wheel){
+            this.wheel = wheel;
+        }
+        public void checkWheel(){
+
+            System.out.println("У машины " + wheel +" колеса");
+
+        }
+}
+
+
+
+
+class Car{
+    private Engine _engine;
+    private Wheel _wheel;
+    private String mark;
+    private int ChangeWheel;
+    public Car(){
+        _engine = new Engine("V8");
+        _wheel = new Wheel(4);
+        mark = "TOYOTA";
+        ChangeWheel = 2;
+    }
+    public void checkMark(){
+        System.out.println("Марка - " + mark);
+    }
+    public void refuel(){
+        System.out.println("Машина заправлена");
+    }
+
+    public void changeWheel(){
+        _wheel.checkWheel();
+        System.out.println("У машины изменено " + ChangeWheel + " колес");
+    }
+
+    public void drive(){
+        System.out.println("Машина может ехать");
+
+    }
+}
