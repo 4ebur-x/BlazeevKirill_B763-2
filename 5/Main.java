6d66a6b8bb9d50a410b46f7ff689b81d2a159208
4ebur-x <kirillblazeev10@gmail.com>
2024-06-23 20:09:21 +0800

    Add files via upload

---
+++ 5/Main.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Main{
+    public static void main(String [] args){
+        Payment payment = new Payment();
+        payment.addItem("Хлеб", 60);
+        payment.addItem("Яйца", 89);
+        payment.addItem("Молоко", 85);
+
+        payment.displayItems();
+        System.out.println("Итоговая цена: " + payment.getTotalPrice() + " Руб.");
+
+
+    }
+
+}
+class Payment {
+
+    private List<Item> items;
+
+    public Payment() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(String name, int price) {
+        Item newItem = new Item(name, price);
+        items.add(newItem);
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (Item item : items) {
+            totalPrice += item.price;
+        }
+        return totalPrice;
+    }
+
+    public void displayItems() {
+        for (Item item : items) {
+            System.out.println("Продукт: " + item.name + " | Цена: " + item.price+ " Руб.");
+        }
+    }
+
+    private class Item {
+        private String name;
+        private int price;
+
+        public Item(String name, int price) {
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
