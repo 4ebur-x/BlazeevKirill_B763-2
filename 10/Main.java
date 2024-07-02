d1bf1f64b93ed599b97763659db04ffd53774711
4ebur-x <kirillblazeev10@gmail.com>
2024-07-02 14:51:49 +0800

    Add files via upload

---
+++ 10/Main.java
@@ -0,0 +1,31 @@
+/*
+Блажеев Кирилл Б763-2
+Вариант А
+1. В каждой строке найти и удалить заданную подстроку.
+*/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String input_filename = "input.txt";
+        String output_filename = "output.txt";
+        String remove = "имя твое";
+        try {
+            FileReader file_read = new FileReader(new File(input_filename));
+            BufferedReader reader = new BufferedReader(file_read);
+            FileWriter file_write = new FileWriter(new File(output_filename));
+            BufferedWriter writer = new BufferedWriter(file_write);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String modified_line = line.replaceAll(remove, "");
+                writer.write(modified_line + "\n");
+            }
+            reader.close();
+            writer.close();
+            System.out.println("Подстроки удалены.");
+        } catch (IOException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+    }
+}
