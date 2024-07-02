d8505319dc4c6f3b9a4c3843beb557b4ef1b4bbe
4ebur-x <kirillblazeev10@gmail.com>
2024-07-02 15:23:59 +0800

    Add files via upload

---
+++ 11/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+
+        Stack<Integer> stack = new Stack<>();
+        System.out.print("Число с цифрами в обратном порядке: ");
+        while (number >  0) {
+            stack.push(number % 10);
+            number = number / 10;
+            System.out.print(stack.pop());
+
+        }
+
+        }
+    }
